235633984f6920b0710d8a23fb433ac676a55cdb
Janner <janner.work@gmail.com>
2021-07-03 13:04:58 -0500

    Cambios menores merge con master

--- src/main/kotlin/models/User.kt
+++ src/main/kotlin/models/User.kt
@@ -5,13 +5,11 @@ var countBook:Int=0
 class User(id:Int, nombre:String, userName:String, password:String, private  var email: String, private var tipoCuenta:String="user",
            var preferredGenre: MutableSet<String> = mutableSetOf<String>()) : Person(id, nombre, userName, password) {
 
-class User(id:Int, nombre:String, userName:String, password:String,private  var email: String,private var tipoCuenta:String="user"): Person(id, nombre, userName, password) {
     fun getEmail():String = email
     fun getTipoCuenta():String = tipoCuenta
 
-    companion object{
-        var tipoCuenta = "user" //definiendo el tipo de cuenta
-    }
-
+        companion object {
+            var tipoCuenta = "user" //definiendo el tipo de cuenta
+        }
 
 }

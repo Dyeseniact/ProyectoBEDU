2f47e43b4815e2cb4aef7bfa386bcfc7565d6904
evega <erick190796@hotmail.com>
2021-07-12 23:02:59 -0500

    Modifiqué la función de login, ahora busca el correo si está en la base de datos y en caso de que sea afirmativo sólo pide rectificar la contraseña. Modificaciones en nombres de variables y funciones respetando la uniformidad del idioma.

--- src/main/kotlin/login.kt
+++ src/main/kotlin/login.kt
@@ -14,8 +14,10 @@ fun createDB(){
 
 }
 
+var runProgram = true
 fun start(){
-    println("""
+    do{
+        println("""
                             -------------------------------
                             |                             |
                             |       -----------|          |
@@ -39,55 +41,84 @@ fun start(){
                             -------------------------------
                         """.trimIndent())
 
-    val seleccion = JOptionPane.showOptionDialog(
-        null ,
-        "¿Es un usuario registrado?",
-        "Login",
-        JOptionPane.YES_NO_CANCEL_OPTION,
-        JOptionPane.QUESTION_MESSAGE,
-        null , arrayOf<Any>("Si", "No", "Salir"),  // null para YES, NO y CANCEL
-        null)
+        val selection = JOptionPane.showOptionDialog(
+            null ,
+            "¿Es un usuario registrado?",
+            "Login",
+            JOptionPane.YES_NO_CANCEL_OPTION,
+            JOptionPane.QUESTION_MESSAGE,
+            null , arrayOf<Any>("Si", "No", "Salir"),  // null para YES, NO y CANCEL
+            null)
 
-    // Verifico si el usuario está registrado o no
-    if(seleccion == 0){
+        // Verifico si el usuario está registrado o no
+        if(selection == 0){
 
-        // Voy a verificar si las credenciasles están correctas
+            // Voy a verificar si las credenciasles están correctas
 
-        var email = retornaRespuesta("Por favor, Introduzca su correo")
-        email = verifyCorrectEmail(email)
+            var email = returnResponseFromCheckboxNotNull("Por favor, Introduzca su correo")
+            email = verifyCorrectEmail(email)
 
-        var password = retornaRespuesta("Por favor, Introduzca su contraseña")
-        password = verifyCorrectPassword(password)
+            var password = returnResponseFromCheckboxNotNull("Por favor, Introduzca su contraseña")
+            password = verifyCorrectPassword(password)
 
-        //Verifica que sea un usuario registrado
-        var loginAnswer = login(email,password)
+            //Verifica que sea un usuario registrado
+            var loginAnswer = login(email,password)
 
-        //Verifica si es un usuario o un administrador
-        when(loginAnswer){
-            "admin" -> menuAdmin()
-            "user" -> menuUser()
-            else -> {
-                JOptionPane.showMessageDialog(null, "No estas registrado" )
-                createUserQuestion()
+            //Verifica si es un usuario o un administrador
+            when(loginAnswer){
+                "admin" -> { menuAdmin(); runProgram = false }
+                "user" -> { menuUser(); runProgram = false }
+                "userWrongPassword" -> do {
+                    var out : Boolean = false
+                    val selection = JOptionPane.showOptionDialog(
+                        null ,
+                        "Hola, parece que has olvidado tu contraseña",
+                        "Contraseña incorrecta",
+                        JOptionPane.YES_NO_CANCEL_OPTION,
+                        JOptionPane.QUESTION_MESSAGE,
+                        null , arrayOf<Any>("Reintentar", "Iniciar sesión con otra cuenta", "Recuperar contraseña"),  // null para YES, NO y CANCEL
+                        null)
+                    when(selection){
+                        0 -> {
+                            password = verifyCorrectPassword(
+                                returnResponseFromCheckboxNotNull("Por favor, Introduzca su contraseña nuevamente")
+                            )
+                            if ( login(email,password) == "user" ) out = true; menuUser();
+                        }
+                        2 -> {
+                            JOptionPane.showMessageDialog(null,
+                                "Hemos enviado un correo con los pasos necesario para recuperar tu contraseña." )
+                        }
+                    }
+                }while (!out)
+                else -> {
+                    JOptionPane.showMessageDialog(null, "No estas registrado" )
+                    createUserQuestion()
+                }
             }
-        }
 
-    }else if (seleccion == 1){
-        createUserQuestion()
-    }else {
-        JOptionPane.showMessageDialog(null, "Adios :(" )
-    }
+        }else if (selection == 1){
+            createUserQuestion()
+        }else {
+            JOptionPane.showMessageDialog(null, "Adios :(" )
+            runProgram = false
+        }
+    }while (runProgram)
 
 }
 fun login(email: String, password: String): String{
     for(i in 0..99){
-        if(listUsr[i]?.getTipoCuenta() == "admin" && listUsr[i]?.getEmail() == email && listUsr[i]?.getPassword() == password){
+        if(listUsr[i]?.getAccount() == "admin" && listUsr[i]?.getEmail() == email && listUsr[i]?.getPassword() == password){
             userLogin = listUsr[i]!!
             return "admin"
         }else{
-            if(listUsr[i]?.getTipoCuenta() == "user" && listUsr[i]?.getEmail() == email && listUsr[i]?.getPassword() == password){
-                userLogin = listUsr[i]!!
-                return "user"
+            if(listUsr[i]?.getAccount() == "user" && listUsr[i]?.getEmail() == email){
+                if( listUsr[i]?.getPassword() == password){
+                    userLogin = listUsr[i]!!
+                    return "user"
+                }else{
+                    return "userWrongPassword"
+                }
             }
         }
     }
@@ -104,7 +135,7 @@ fun verifyCorrectEmail(email:String): String {
         if (!correctEmail) {
             JOptionPane.showMessageDialog(null,
                 "El correo no es correcto hace falta el @, ingresa nuevamente el correo." )
-            correo = retornaRespuesta("Ingresa email nuevamente")
+            correo = returnResponseFromCheckboxNotNull("Ingresa email nuevamente")
         }
     } while (!correctEmail)
     return correo
@@ -118,14 +149,14 @@ fun verifyCorrectPassword(password:String):String{
         if(!correctPassword){
             JOptionPane.showMessageDialog(null,
                 "La contraseña al menos debe tener 8 caracteres, ingresa nuevamente la contraseña." )
-            pass = retornaRespuesta("Ingrese tu contraseña")
+            pass = returnResponseFromCheckboxNotNull("Ingrese tu contraseña")
         }
     }while(!correctPassword)
     return pass
 }
 
 fun createUserQuestion(){
-    val seleccion = JOptionPane.showOptionDialog(
+    val selection = JOptionPane.showOptionDialog(
         null ,
         "¿Desea crear un usuario?",
         "Creando ususario",
@@ -133,7 +164,7 @@ fun createUserQuestion(){
         JOptionPane.QUESTION_MESSAGE,
         null , arrayOf<Any>("Si", "No"),  // null para YES, NO y CANCEL
         null)
-    if(seleccion == 0){
+    if(selection == 0){
         createUsr()
         menuUser()
     }else{
@@ -170,17 +201,17 @@ fun createUsr(){
                             -------------------------------
                         """.trimIndent())
 
-    val name= retornaRespuesta("Favor de ingresar su nombre")
-    val userName = retornaRespuesta("Favor de ingresar su user name")
-    val password = verifyCorrectPassword(retornaRespuesta("Favor de ingresar su contraseña"))
-    val email = verifyCorrectEmail(retornaRespuesta("Favor de ingresar su correo"))
+    val name= returnResponseFromCheckboxNotNull("Favor de ingresar su nombre")
+    val userName = returnResponseFromCheckboxNotNull("Favor de ingresar su user name")
+    val password = verifyCorrectPassword(returnResponseFromCheckboxNotNull("Favor de ingresar su contraseña"))
+    val email = verifyCorrectEmail(returnResponseFromCheckboxNotNull("Favor de ingresar su correo"))
 
     do{
         var out: Boolean=false
-        val num = retornaRespuesta("Favor de ingresar número")
+        val num = returnResponseFromCheckboxNotNull("Favor de ingresar número")
         try {
-            val numero = num.toLong()
-            if(numero<1000000000){
+            val numberPhone = num.toLong()
+            if(numberPhone<1000000000){
                 JOptionPane.showMessageDialog(null,
                     "El número es menor a 10 dígitos" )
             }else{
@@ -267,9 +298,9 @@ fun selectPreferredGenre(user: User?=null){
         """.trimIndent()
         )
 
-        var respuesta: String?
+        var response: String?
         do{
-            respuesta = when(retornaRespuesta("¿Qué te gusta leer?\n 1.Libros\n 2.Revistas\n 3.Artículos \n \n 0.Skip \n" +
+            response = when(returnResponseFromCheckboxNotNull("¿Qué te gusta leer?\n 1.Libros\n 2.Revistas\n 3.Artículos \n \n 0.Skip \n" +
                     "\n Nota: En caso de que sean más de uno ingresar los números juntos")){
                 "1" -> "1"
                 "2" -> "2"
@@ -281,9 +312,9 @@ fun selectPreferredGenre(user: User?=null){
                 "13" ->"13"
                 else -> null
             }
-        }while( respuesta==null )
+        }while( response==null )
 
-        respuesta.toCharArray().map {
+        response.toCharArray().map {
             when( it ){
                 '1' ->{
                     println("""
@@ -309,7 +340,7 @@ fun selectPreferredGenre(user: User?=null){
                             |                  Skip ->    |
                             -------------------------------
                         """.trimIndent())
-                    genreSelected = seleccionaLosGéneros(genreSelected,allGenres,0, arrayListOf(1,2,3,4,0))
+                    genreSelected = genredSelect(genreSelected,allGenres,0, arrayListOf(1,2,3,4,0))
                 }
                 '2' ->{
                     println("""
@@ -335,7 +366,7 @@ fun selectPreferredGenre(user: User?=null){
                             |                  Skip ->    |
                             -------------------------------
                         """.trimIndent())
-                    genreSelected = seleccionaLosGéneros(genreSelected,allGenres,1, arrayListOf(1,2,3,4,5,6,7,0))
+                    genreSelected = genredSelect(genreSelected,allGenres,1, arrayListOf(1,2,3,4,5,6,7,0))
                 }
                 '3' ->{
                     println("""
@@ -361,12 +392,12 @@ fun selectPreferredGenre(user: User?=null){
                             |                  Skip ->    |
                             -------------------------------
                         """.trimIndent())
-                    genreSelected = seleccionaLosGéneros(genreSelected,allGenres,2, arrayListOf(1,2,3,0))
+                    genreSelected = genredSelect(genreSelected,allGenres,2, arrayListOf(1,2,3,0))
                 }
             }
         }
 
-        val salir = when(JOptionPane.showOptionDialog(
+        val getOut = when(JOptionPane.showOptionDialog(
             null ,
             "Muchas gracias por seleccionar los géneros preferidos",
             "Mi lectura favorita",
@@ -377,36 +408,36 @@ fun selectPreferredGenre(user: User?=null){
             1 -> true
             else -> false
         }
-    }while(salir)
+    }while(getOut)
     user?.preferredGenre = genreSelected
 }
 
-fun retornaRespuesta(mensaje:String):String{
+fun returnResponseFromCheckboxNotNull(mensaje:String):String{
     //Esta función permite obtener una respuesta que no sea nula
-    var respuesta:String?
-    do { respuesta = JOptionPane.showInputDialog(mensaje)
-    }while (respuesta==null)
-    return respuesta
+    var response:String?
+    do { response = JOptionPane.showInputDialog(mensaje)
+    }while (response==null)
+    return response
 }
 
-fun seleccionaLosGéneros(genreSelected: ArrayList<MutableSet<String>>,
-                         allGenres: ArrayList<MutableSet<String>>, tipo:Int,
-                         listaOpciones:ArrayList<Int>): ArrayList<MutableSet<String>> {
-    var respuesta: String
+fun genredSelect(genreSelected: ArrayList<MutableSet<String>>,
+                 allGenres: ArrayList<MutableSet<String>>, tipo:Int,
+                 optionslist:ArrayList<Int>): ArrayList<MutableSet<String>> {
+    var response: String
     do{
-        respuesta = retornaRespuesta("Ingresa tus géneros preferidos \n" +
+        response = returnResponseFromCheckboxNotNull("Ingresa tus géneros preferidos \n" +
                 " 0.Skip \n" +
                 "\n Nota: En caso de que sean más de uno ingresar los números juntos en orden ")
         var viable = false
-        respuesta?.forEach { char ->
-            listaOpciones.map { int ->
+        response?.forEach { char ->
+            optionslist.map { int ->
                 viable = char.toString().toInt() == int; if(viable) return@forEach
             }
         }
     }while( viable==false )
 
-    if(respuesta!!.toInt() != 0){
-        respuesta!!.forEach {valor ->
+    if(response!!.toInt() != 0){
+        response!!.forEach { valor ->
             genreSelected[tipo].add(allGenres[tipo].elementAt(valor.toString().toInt()-1))
         }
         return genreSelected
--- src/main/kotlin/models/User.kt
+++ src/main/kotlin/models/User.kt
@@ -6,10 +6,10 @@ var listMagazine = Array<Magazine?>(100){null}
 var countMagazine:Int=0
 var listArticle = Array<Article?>(100){null}
 var countArticle:Int=0
-class User(id:Int, nombre:String, userName:String, password:String, private  var email: String, private var tipoCuenta:String="user",
+class User(id:Int, nombre:String, userName:String, password:String, private  var email: String, private var accountType:String="user",
            var preferredGenre: ArrayList<MutableSet<String>> = arrayListOf()) : Person(id, nombre, userName, password) {
 
     fun getEmail():String = email
-    fun getTipoCuenta():String = tipoCuenta
+    fun getAccount():String = accountType
 
 }
